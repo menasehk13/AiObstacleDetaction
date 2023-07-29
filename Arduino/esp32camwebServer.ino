f2b5c44bb14692c8199f489f2eaa975bae01dd38
menasehk13 <menasehk13@gmail.com>
2023-07-29 05:08:59 +0300

    esp32Ardunio code added

--- Arduino/esp32camwebServer.ino
+++ Arduino/esp32camwebServer.ino
@@ -1,78 +1,125 @@
 #include "esp_camera.h"
-#include "WiFi.h"
-#include "WebSocketsServer.h"
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-const char* ssid = "zed";
+const int in1 = 12;
+const int in2 = 13;
+const int enA = 15;
+const int in3 = 14;
+const int in4 = 2;
+const int enB= 15;
+int leftSpeed = 200;
+int rightSpeed = 200;
+const char* ssid     = "zed";
 const char* password = "12615776";
-const int webServerPort = 80;
-WiFiServer server(webServerPort);
-const int webSocketPort = 81;
-WebSocketsServer webSocket = WebSocketsServer(webSocketPort);
-TaskHandle_t webSocketTaskHandle = NULL;
-TaskHandle_t videoStreamingTaskHandle = NULL;
-void webSocketTask(void *pvParameters);
-void videoStreamingTask(void *pvParameters);
 
-void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
-  if (type == WStype_TEXT) {
-    String message = String((char*)payload);
-    if (message == "f") {
-      Serial.println("Forward");
-    } else if (message == "l") {
-      Serial.println("Left");
-    } else if (message == "r") {
-     Serial.println("Right");
-    } else if (message == "d") {
-      Serial.println("Down");
-    } else {
-     Serial.println("speed controller");
-    }
-  }
-}
-void webSocketTask(void *pvParameters) {
-  while (1) {
-    webSocket.loop();
-    delay(1);
-  }
-}
+AsyncWebServer server(80);
+AsyncWebSocket wsCamera("/Camera");
+AsyncWebSocket wsCarInput("/CarInput");
+uint32_t cameraClientId = 0;
+void onCarInputWebSocketEvent(AsyncWebSocket *server,
+                              AsyncWebSocketClient *client,
+                              AwsEventType type,
+                              void *arg,
+                              uint8_t *data,
+                              size_t len)
+{
+  switch (type)
+  {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
 
-void videoStreamingTask(void *pvParameters) {
-  while (1) {
-    WiFiClient client = server.available();
-    if (client) {
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: multipart/x-mixed-replace; boundary=frame");
-      client.println();
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      Stop(); // Stop the car when the WebSocket client disconnects
+      break;
 
-      while (true) {
-        camera_fb_t *fb = esp_camera_fb_get();
-        if (!fb) {
-          Serial.println("Camera capture failed");
-          break;
+    case WS_EVT_DATA:
+      AwsFrameInfo *info;
+      info = (AwsFrameInfo*)arg;
+      if (info->opcode == WS_TEXT && len == 1)
+      {
+        char command = data[0];
+        Serial.printf("Received command: %c\n", command);
+
+        // Process the received command
+        switch (command)
+        {
+          case 'f':
+          Serial.print("f"); // Forward
+            moveForward();
+            break;
+
+          case 'b':
+          Serial.print("f");  // Backward
+            moveBackward();
+            break;
+
+          case 'l':
+          Serial.print("f");  // Left
+            moveLeft();
+            break;
+
+          case 'r':
+          Serial.print("f");  // Right
+            moveRight();
+            break;
+
+          case 's': // Stop
+            Serial.print("f");
+            Stop();
+            break;
+
+          default:
+            Serial.println("Unknown command");
+            break;
         }
-        client.print("--frame\r\n");
-        client.print("Content-Type: image/jpeg\r\n");
-        client.print("Content-Length: " + String(fb->len) + "\r\n");
-        client.println("Cache-Control: no-store");
-        client.print("\r\n");
-        client.write(fb->buf, fb->len);
-        client.print("\r\n");
-        esp_camera_fb_return(fb);
       }
-    }
-    delay(1);
+      break;
+
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+
+    default:
+      break;
   }
 }
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+
+void onCameraWebSocketEvent(AsyncWebSocket *server,
+                            AsyncWebSocketClient *client,
+                            AwsEventType type,
+                            void *arg,
+                            uint8_t *data,
+                            size_t len)
+{
+switch (type)
+  {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      cameraClientId = client->id();
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      cameraClientId = 0;
+      break;
+    case WS_EVT_DATA:
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+    default:
+      break;
   }
+}
 
-  camera_config_t config;
+void setupCamera()
+{
+ camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
   config.pin_d0 = 5;
@@ -105,34 +152,131 @@ void setup() {
   }
 
   Serial.println("Camera initialized!");
+}
+
+void sendCameraPicture()
+{
+  if (cameraClientId == 0)
+  {
+    return;
+  }
+  unsigned long  startTime1 = millis();
+  //capture a frame
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb)
+  {
+      Serial.println("Frame buffer could not be acquired");
+      return;
+  }
+
+  unsigned long  startTime2 = millis();
+  wsCamera.binary(cameraClientId, fb->buf, fb->len);
+  esp_camera_fb_return(fb);
+
+  //Wait for message to be delivered
+  while (true)
+  {
+    AsyncWebSocketClient * clientPointer = wsCamera.client(cameraClientId);
+    if (!clientPointer || !(clientPointer->queueIsFull()))
+    {
+      break;
+    }
+    delay(1);
+  }
+
+  unsigned long  startTime3 = millis();
+  Serial.printf("Time taken Total: %d|%d|%d\n",startTime3 - startTime1, startTime2 - startTime1, startTime3-startTime2 );
+}
+void setupPins(){
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enA, OUTPUT);
+   pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(enB, OUTPUT);
+}
+
+void setup() {
+
+  Serial.begin(115200);
+   WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
   Serial.print("ESP32 local IP: ");
   Serial.println(WiFi.localIP());
+  wsCamera.onEvent(onCameraWebSocketEvent);
+  server.addHandler(&wsCamera);
+
+  wsCarInput.onEvent(onCarInputWebSocketEvent);
+  server.addHandler(&wsCarInput);
 
   server.begin();
-  webSocket.begin();
-  webSocket.onEvent(onWebSocketEvent);
-
-  xTaskCreatePinnedToCore(
-      webSocketTask,
-      "WebSocketTask",
-      10000,
-      NULL,
-      1,
-      &webSocketTaskHandle,
-      0);
-
-
-  xTaskCreatePinnedToCore(
-      videoStreamingTask,
-      "VideoStreamingTask",
-      10000,
-      NULL,
-      1,
-      &videoStreamingTaskHandle,
-      1);
+  Serial.println("HTTP server started");
+  setupPins();
+  setupCamera();
 }
 
+void moveForward(){
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+   digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, leftSpeed);
+    analogWrite(enB, rightSpeed);
+
+}
+void moveLeft() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, leftSpeed / 2);
+    analogWrite(enB, rightSpeed);
+     delay(500);
+  Stop();
+}
+
+void moveRight() {
+   digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enA, leftSpeed);
+    analogWrite(enB, rightSpeed / 2);
+     delay(500);
+  Stop();
+}
+
+void moveBackward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+   digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enA, leftSpeed);
+    analogWrite(enB, rightSpeed);
+}
+
+void Stop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
+}
+
+void setSpeed(int newSpeed) {
+  leftSpeed = constrain(newSpeed, 0, 255);
+  rightSpeed = constrain(newSpeed, 0, 255);
+}
 void loop() {
+   wsCamera.cleanupClients();
+  wsCarInput.cleanupClients();
+  sendCameraPicture();
 
 }
